85454781937e510e1c7f8b8e599860e98c04f7b1
Sai Asish Y <say.apm35@gmail.com>
2026-08-24 17:45:29 -0700

    fix escape() TypeError on ascii bytes
    Signed-off-by: Sai Asish Y <say.apm35@gmail.com>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,4 +10,5 @@ def test_escape_normalizes_values():
     assert escape(None) == ''
     assert escape(42) == '42'
     assert escape(b'pi\xc3\xb9 <b>') == 'pi\u00f9 &lt;b&gt;'
+    assert escape(b'hello') == 'hello'
     assert escape('say "hi"', quote=True) == 'say &quot;hi&quot;'
